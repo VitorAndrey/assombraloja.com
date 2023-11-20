e839c75fb1a382258893157a0e59061d05fd1f0d
Vitor Andrey <vitor.andreylopess@gmail.com>
2023-11-20 16:39:00 -0300

    fix: add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 
 import { ThemeProvider } from "@/components/theme-provider";
 
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
 import { cn } from "@/lib/utils";
 import "../styles/globals.css";
 
@@ -18,7 +24,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={cn("bg-zinc-50 min-h-screen font-sans antialiased w-full")}
+        className={cn(
+          "bg-zinc-50 min-h-screen font-sans antialiased w-full",
+          inter.className,
+        )}
       >
         <ThemeProvider attribute="class" defaultTheme="light">
           {children}
