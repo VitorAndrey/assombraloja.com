e79b86298f7932766e3a5fbb1ee4ffaeb5046b66
Vitor Andrey <vitor.andreylopess@gmail.com>
2023-11-20 16:25:44 -0300

    fix: simplify login route

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,29 +1,5 @@
-// import prisma from "@/lib/prisma";
-// import { z } from "zod";
+import { NextResponse } from "next/server";
 
-// export default async function handler(req, res) {
-//   if (req.method === "POST") {
-//     const bodySchema = z.object({
-//       email: z.string(),
-//       password: z.string(),
-//     });
-
-//     const { email, password } = bodySchema.parse(req.body);
-
-//     const user = await prisma.user.findUnique({
-//       where: {
-//         email: email,
-//       },
-//     });
-
-//     if (!user || user.password !== password) {
-//       return res.status(401).json({ error: "Invalid credentials" });
-//     }
-
-//     // Here you should handle creating a session or a JWT token for the authenticated user
-
-//     return res.status(200).json({ message: "Login successful" });
-//   } else {
-//     return res.status(405).json({ error: "Method not allowed" });
-//   }
-// }
+export async function GET(request: Request) {
+  return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+}
